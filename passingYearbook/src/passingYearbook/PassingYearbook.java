bc813fdc25f0f9b6bd4c12c8c5968dbcc55650cd
echaro <echaro08@gmail.com>
2021-10-04 19:42:07 -0600

    Passing yearbook solution2 /init matching pairs

--- passingYearbook/src/passingYearbook/PassingYearbook.java
+++ passingYearbook/src/passingYearbook/PassingYearbook.java
@@ -1,7 +1,11 @@
 package passingYearbook;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -29,12 +33,38 @@ public class PassingYearbook {
 		}
 		return res;
 	}
+	
+	public int[] findSignatureCounts2(int[] arr) {
+	    int[] result = new int[arr.length];
+	    List<LinkedHashSet<Integer>> loops = new ArrayList<LinkedHashSet<Integer>>();
+
+	    Arrays.fill(result, 0);
+	    for (int i = 0; i < arr.length; i++) {
+	        for(LinkedHashSet<Integer> loop : loops) {
+	            if(loop.contains(arr[i])) {
+	                result[i]=loop.size();
+	                break;
+	            }
+	        }
+	    
+	        if(result[i]==0) {
+	            LinkedHashSet<Integer> loop = new LinkedHashSet<Integer>();
+	            loop.add(arr[i]);
+	            int index = arr[i] - 1;
+	            while (index != i) {
+	                loop.add(arr[index]);
+	                index = arr[index] - 1;
+	            }
+	            result[i] = loop.size();
+	        }
+	    }
+	    return result;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		PassingYearbook passing = new PassingYearbook();
-		int[] nums1 = {2, 1};
-		int[] nums2 = {1, 2};
-		passing.findSignatureCounts(nums1);
+		int[] nums1 = {5,3,9,4,1,8,6,2,7};
+		passing.findSignatureCounts2(nums1);
 
 	}
 
